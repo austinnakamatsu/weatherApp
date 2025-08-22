8efa5c265a9fc2044120aeeb7d5b7c88c83e8715
Austin Nakamatsu <nakamata@oregonstate.edu>
2025-08-22 11:07:27 -1000

    Debug deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import FetchSearch from './pages/Search'
@@ -10,14 +10,10 @@ import './index.css'
 
 const queryClient = new QueryClient()
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <FetchSearch />,
-        children: [
-            { path: "fetch-search", element: <FetchSearch /> },
-            { index: true, element: <Navigate to="/fetch-search" replace /> }
-        ]
     }
 ])
 
@@ -26,5 +22,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
         </QueryClientProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
 )
